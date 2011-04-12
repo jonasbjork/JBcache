1885ab6faa3f4f2863b34c3768d519da1cc7a454
David V. Wallin <david@egib.(none)>
2011-04-12 19:09:33 +0200

    standard test-file for master-branch

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  require_once('JBCache.class.php');
+  $cache = new JBCache();
+  $cache->start($_SERVER['REQUEST_URI']);
+?>
+<body>
+<p>If you had an crystal ball you could see that...</p>
+<?php
+  for ($i=0; $i<20; $i++) {
+          printf("The magic number is: %d<br />", $i);
+  }
+?>
+<p>No wonder we want an crystal ball, right?</p>
+<!-- Really cool code, right? ;) -->
+</body>
+<?php
+  if ($cache->has_cache()) {
+          $cache->stop($_SERVER['REQUEST_URI']);
+  }
+?>
